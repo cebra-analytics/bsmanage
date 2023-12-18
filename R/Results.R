1c988af30b3498c48dd0bf956b00171cea10443c
seanhaythorne <seanhaythorne@hotmail.com>
2023-12-18 14:38:40 +1100

    Initial extended simulator and result classes

---
+++ R/Results.R
@@ -0,0 +1,168 @@
+#' Results class builder
+#'
+#' Builds a class for encapsulating, calculating, and collating incursion
+#' management simulation results, including the population at each location at
+#' each collation time step, the total population size and area occupied at
+#' each time step, as well as spatio-temporal impacts and management action
+#' quantities applied (e.g. removals). When simulations are replicated, summary
+#' results (means and standard deviations) are produced. Extends the
+#' \code{bsspread::Results} package module.
+#'
+#' @param region A \code{bsspread::Region} or inherited class object
+#'   representing the spatial region (template) for the incursion management
+#'   simulations.
+#' @param population_model A \code{bsspread::Population} or inherited class
+#'   object defining the population representation and growth functionality for
+#'   the incursion management simulations.
+#' @param time_steps The number of discrete time steps to simulate. Default is
+#'   1.
+#' @param step_duration The duration of the simulation time steps in units
+#'   specified by \code{step_units}. Default is 1.
+#' @param step_units The units for the simulation step duration
+#'   (\code{step_duration}) as a character string. Default is "years".
+#' @param collation_steps The interval in time steps for collating results.
+#'   Default is 1, that is, results are collated at every time step.
+#' @param replicates The number of replicate or repeated simulations to be run.
+#'   Default is 1. Note that replicate simulations results are collated as
+#'   summary statistics across simulations.
+#' @param combine_stages Optionally combine (sum) specified stages (a vector of
+#'   stage indices) of stage-based population results. The default
+#'   (\code{NULL}) maintains results for each stage.
+#' @param ... Additional parameters.
+#' @return A \code{Results} class object (list) containing functions for
+#'   calculating and collating results, as well as accessing lists of results
+#'   and the simulation parameters used to produce them:
+#'   \describe{
+#'     \item{\code{collate(r, tm, n)}}{Collate the results at simulation
+#'       replicate \code{r} and time step \code{tm} using the current vector or
+#'       array \code{n} representing the population at each location.}
+#'     \item{\code{finalize()}}{Finalize the results collation (summary
+#'       calculations).}
+#'     \item{\code{as_list()}}{Return the results as a list (collated, total,
+#'       area).}
+#'     \item{\code{get_params()}}{Get the simulation parameters used.}
+#'     \item{\code{save_rasters(...)}}{Save the collated results as raster TIF
+#'       files when the region is grid-based. \code{Terra} raster write options
+#'       may be passed to the function.}
+#'     \item{\code{save_csv()}}{Save the collated results as comma-separated
+#'       values (CSV) files when the region is patch-based. Also saves the
+#'       population totals and area occupied to CSV files for both grid and
+#'       patch-based region types.}
+#'     \item{\code{save_plots()}}{Save plots of the population (staged) totals
+#'       and the area occupied as PNG files.}
+#'   }
+#' @references
+#'   Bradhurst, R., Spring, D., Stanaway, M., Milner, J., & Kompas, T. (2021).
+#'   A generalised and scalable framework for modelling incursions,
+#'   surveillance and control of plant and environmental pests.
+#'   \emph{Environmental Modelling & Software}, 139, N.PAG.
+#'   \doi{10.1016/j.envsoft.2021.105004}
+#'
+#'   García Adeva, J. J., Botha, J. H., & Reynolds, M. (2012). A simulation
+#'   modelling approach to forecast establishment and spread of Bactrocera
+#'   fruit flies. \emph{Ecological Modelling}, 227, 93–108.
+#'   \doi{10.1016/j.ecolmodel.2011.11.026}
+#' @export
+Results <- function(region, population_model,
+                    time_steps = 1,
+                    step_duration = 1,
+                    step_units = "years",
+                    collation_steps = 1,
+                    replicates = 1,
+                    combine_stages = NULL, ...) {
+  UseMethod("Results")
+}
+
+#' @name Results
+#' @export
+Results.Region <- function(region, population_model,
+                           time_steps = 1,
+                           step_duration = 1,
+                           step_units = "years",
+                           collation_steps = 1,
+                           replicates = 1,
+                           combine_stages = NULL, ...) {
+
+  # Create a bsspread::Results class structure (includes validation)
+  super <- bsspread::Results(region, population_model,
+                             time_steps = time_steps,
+                             step_duration = step_duration,
+                             step_units = step_units,
+                             collation_steps = collation_steps,
+                             replicates = replicates,
+                             combine_stages = combine_stages, ...)
+
+  # Population stages (NULL or number of stages)
+  stages <- population_model$get_stages()
+
+  # Initialize additional incursion management result lists
+  results <- list()
+  # TODO
+
+  # Create a class structure
+  self <- structure(list(), class = "Results")
+
+  # Extended collate results
+  self$collate <- function(r, tm, n) {
+
+    # Collate population spread results
+    super$collate(r, tm, n)
+
+    # Collate additional incursion management results
+    # TODO
+  }
+
+  # Extended finalize the results collation
+  self$finalize <- function() {
+
+    # Finalize population spread results
+    super$finalize()
+
+    # Finalize additional incursion management results
+    # TODO
+  }
+
+  # Get results list
+  self$get_list <- function() {
+    return(c(super$get_list(), results))
+  }
+
+  # Get simulation parameters
+  self$get_params  <- function() {
+    return(super$get_params(), list())
+  }
+
+  # Save collated results as raster files
+  if (region$get_type() == "grid") {
+    self$save_rasters  <- function(...) {
+
+      # Save population spread results
+      super$save_rasters()
+
+      # Save additional incursion management results
+      # TODO
+    }
+  }
+
+  # Save collated (patch only) and summary (both) results as CSV files
+  self$save_csv  <- function() {
+
+    # Save population spread results
+    super$save_csv()
+
+    # Save additional incursion management results
+    # TODO
+  }
+
+  # Extended save plots as PNG files
+  self$save_plots  <- function() {
+
+    # Save population spread results
+    super$save_plots()
+
+    # Save additional incursion management results
+    # TODO
+  }
+
+  return(self)
+}
---
+++ R/Simulator.R
@@ -0,0 +1,222 @@
+#' Simulator class builder
+#'
+#' Builds a class to configure and run replicate discrete-time incursion
+#' management simulations over a given spatial region using (sub)models for
+#' population growth, dispersal, impacts, detection, and management. Extends
+#' the \code{bsspread::Simulator} package module.
+#'
+#' @param region A \code{raster::RasterLayer}, \code{terra::SpatRaster}, or
+#'   \code{bsspread::Region} or inherited class object representing the spatial
+#'   region (template) for the incursion management simulations.
+#' @param time_steps The number of discrete time steps to simulate. Default is
+#'   1.
+#' @param step_duration The duration of the simulation time steps in units
+#'   specified by \code{step_units}. Default is 1.
+#' @param step_units The units for the simulation step duration
+#'   (\code{step_duration}) as a character string. Default is "years".
+#' @param collation_steps The interval in time steps for collating results.
+#'   Default is 1, that is, results are collated at every time step.
+#' @param replicates The number of replicate or repeated simulations to be run.
+#'   Default is 1. Note that replicate simulations results are collated as
+#'   summary statistics across simulations.
+#' @param result_stages Optionally combine (sum) specified stages (a vector of
+#'   stage indices) of stage-based population results. The default
+#'   (\code{NULL}) maintains results for each stage.
+#' @param parallel_cores Number of cores available for parallel processing.
+#'   The default NULL implies no parallel processing.
+#' @param initializer A \code{bsspread::Initializer} or inherited class object
+#'   for generating the initial invasive species population distribution or
+#'   incursion locations, as well as optionally generating subsequent
+#'   incursions during the incursion management simulations.
+#' @param population_model A \code{bsspread::Population} or inherited class
+#'   object defining the population representation and growth functionality for
+#'   the incursion management simulations.
+#' @param dispersal_models A list of \code{bsspread::Dispersal} or inherited
+#'   class objects defining the dispersal functionality for the different
+#'   spread vectors to be simulated.
+#' @param user_function An optional user-defined \code{function(n)} that is
+#'   applied to the population vector or matrix \code{n} (returning a
+#'   transformed \code{n}) prior to collating the results at each simulation
+#'   time step.
+#' @param ... Additional parameters.
+#' @return A \code{Simulator} class object (list) containing functions for
+#'   setting objects (in the function environment) and running the simulations:
+#'   \describe{
+#'     \item{\code{set_initializer(object)}}{Set the initializer object.}
+#'     \item{\code{set_population_model(model)}}{Set the population model
+#'       object.}
+#'     \item{\code{set_dispersal_models(models)}}{Set the list of dispersal
+#'       model objects.}
+#'     \item{\code{run()}}{Run the simulations and return the results.}
+#'   }
+#' @references
+#'   Bradhurst, R., Spring, D., Stanaway, M., Milner, J., & Kompas, T. (2021).
+#'   A generalised and scalable framework for modelling incursions,
+#'   surveillance and control of plant and environmental pests.
+#'   \emph{Environmental Modelling & Software}, 139, N.PAG.
+#'   \doi{10.1016/j.envsoft.2021.105004}
+#'
+#'   García Adeva, J. J., Botha, J. H., & Reynolds, M. (2012). A simulation
+#'   modelling approach to forecast establishment and spread of Bactrocera
+#'   fruit flies. \emph{Ecological Modelling}, 227, 93–108.
+#'   \doi{10.1016/j.ecolmodel.2011.11.026}
+#' @include Results.R
+#' @export
+Simulator <- function(region,
+                      time_steps = 1,
+                      step_duration = 1,
+                      step_units = "years",
+                      collation_steps = 1,
+                      replicates = 1,
+                      result_stages = NULL,
+                      parallel_cores = NULL,
+                      initializer = NULL,
+                      population_model = NULL,
+                      dispersal_models = list(),
+                      user_function = NULL, ...) {
+  UseMethod("Simulator")
+}
+
+#' @name Simulator
+#' @export
+Simulator.Raster <- function(region, ...) {
+  # Call Region class version
+  Simulator(bsspread::Region(region), ...)
+}
+
+#' @name Simulator
+#' @export
+Simulator.SpatRaster <- function(region, ...) {
+  # Call Region class version
+  Simulator(bsspread::Region(region), ...)
+}
+
+#' @name Simulator
+#' @export
+Simulator.Region <- function(region,
+                             time_steps = 1,
+                             step_duration = 1,
+                             step_units = "years",
+                             collation_steps = 1,
+                             replicates = 1,
+                             result_stages = NULL,
+                             parallel_cores = NULL,
+                             initializer = NULL,
+                             population_model = NULL,
+                             dispersal_models = list(),
+                             user_function = NULL, ...) {
+
+  # Create a bsspread::Simulator class structure (includes validation)
+  self <- bsspread::Simulator(region,
+                              time_steps = time_steps,
+                              step_duration = step_duration,
+                              step_units = step_units,
+                              collation_steps = collation_steps,
+                              replicates = replicates,
+                              result_stages = result_stages,
+                              parallel_cores = parallel_cores,
+                              initializer = initializer,
+                              population_model = population_model,
+                              dispersal_models = dispersal_models,
+                              user_function = user_function, ...)
+
+  # Extend (override) run simulator function
+  results <- NULL # DEBUG ####
+  self$run <- function() {
+
+    # Should at least have an initializer and a population model
+    object_absence <- c(is.null(initializer), is.null(population_model))
+    if (any(object_absence)) {
+      stop(sprintf("The simulator requires the %s to be set.",
+                   paste(c("initializer", "population model")[object_absence],
+                         collapse = " and ")),
+           call. = FALSE)
+    }
+
+    # Continued incursions function
+    continued_incursions <- initializer$continued_incursions()
+
+    # Results setup
+    results <<- Results(region, population_model, # DEBUG ####
+                        time_steps = time_steps,
+                        step_duration = step_duration,
+                        step_units = step_units,
+                        collation_steps = collation_steps,
+                        replicates = replicates,
+                        combine_stages = result_stages)
+
+    # Replicates
+    for (r in 1:replicates) {
+
+      # Initialize population array
+      n <- initializer$initialize()
+
+      # Set diffusion attributes when spatially implicit (single patch)
+      if (region$spatially_implicit()) {
+        attr(n, "initial_n") <- n
+        attr(n, "diffusion_rate") <-
+          unlist(lapply(dispersal_models, function(dm) {
+          if (inherits(dm, "Diffusion")) {
+            dm$get_diffusion_rate()
+          }
+        }))[1] # assume one only
+        if (is.numeric(attr(n, "diffusion_rate"))) {
+          attr(n, "diffusion_radius") <- 0
+        }
+      }
+
+      # Initial results (t = 0)
+      results$collate(r, 0, n)
+
+      # Time steps
+      for (tm in 1:time_steps) {
+
+        # Set time step attribute when spatially implicit (single patch)
+        if (region$spatially_implicit()) {
+          attr(n, "tm") <- tm
+        }
+
+        # Population growth
+        n <- population_model$grow(n)
+
+        # Dispersal for each spread vector
+        if (length(dispersal_models)) {
+
+          # Pack into list of original, remaining and relocated populations
+          n <- dispersal_models[[1]]$pack(n)
+
+          # Perform dispersal for each spread vector
+          for (i in 1:length(dispersal_models)) {
+            n <- dispersal_models[[i]]$disperse(n)
+          }
+
+          # Unpack population array from separated list
+          n <- dispersal_models[[1]]$unpack(n)
+        }
+
+        # User-defined function
+        if (is.function(user_function)) {
+          n <- user_function(n)
+        }
+
+        # Collate results
+        results$collate(r, tm, n)
+
+        # Continued incursions
+        if (is.function(continued_incursions)) {
+          n <- continued_incursions(tm, n)
+        }
+
+      } # time steps
+
+    } # replicates
+
+    # Finalize results
+    results$finalize()
+
+    # Return results
+    return(results)
+  }
+
+  return(self)
+}
