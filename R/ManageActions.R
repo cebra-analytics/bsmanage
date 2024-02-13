488b15a25adc217a18efa8e5d1d4e800259b8469
seanhaythorne <seanhaythorne@hotmail.com>
2024-02-13 15:12:49 +1100

    Added generic manage actions class

---
+++ R/ManageActions.R
@@ -0,0 +1,71 @@
+#' Manage actions class builder
+#'
+#' Builds a generic class for simulating the application of management actions,
+#' such as detection, control, and removal.
+#'
+#' @param region A \code{bsspread::Region} or inherited class object
+#'   representing the spatial region (template) for the incursion management
+#'   simulations.
+#' @param population_model A \code{bsspread::Population} or inherited class
+#'   object defining the population representation for the management
+#'   simulations.
+#' @param apply_stages Numeric vector of population stages (indices) to which
+#'   management actions are applied. Default is all stages (when set to
+#'   \code{NULL}).
+#' @param ... Additional parameters.
+#' @return A \code{ManageActions} class object (list) containing a function for
+#'   applying simulated management actions:
+#'   \describe{
+#'     \item{\code{apply(n)}}{Apply management actions to a simulated
+#'       population vector or matrix \code{n}, potentially with attached
+#'       attributes relating to previously applied actions, and return the
+#'       resulting population \code{n} along with attached attributes relating
+#'       to the newly applied actions.}
+#'   }
+#' @export
+ManageActions <- function(region, population_model,
+                          apply_stages = NULL,
+                          class = character(), ...) {
+  UseMethod("ManageActions")
+}
+
+#' @name ManageActions
+#' @export
+ManageActions.Region <- function(region, population_model,
+                                 apply_stages = NULL,
+                                 class = character(), ...) {
+
+  # Check the population model
+  if (!is.null(population_model) &&
+      !inherits(population_model, "Population")) {
+    stop("Population model must be a 'Population' or inherited class object.",
+         call. = FALSE)
+  } else if (nrow(as.matrix(population_model$make(0))) !=
+             region$get_locations()) {
+    stop("Population model must be compatible with the region object.",
+         call. = FALSE)
+  }
+
+  # Check the applied stages
+  population_type <- population_model$get_type()
+  if (population_type == "presence_only" ||
+      population_type == "unstructured") {
+    apply_stages <- 1
+  } else if (population_type == "stage_structured") {
+    if (is.null(apply_stages)) {
+      apply_stages <- 1:population_model$get_stages()
+    } else if (!is.numeric(apply_stages) ||
+               !all(apply_stages %in% 1:population_model$get_stages())) {
+      stop("Apply stages must be a vector of stage indices consistent ",
+           "with the population model.", call. = FALSE)
+    }
+  }
+
+  # Create a class structure
+  self <- structure(list(), class = c(class, "ManageActions"))
+
+  # Generic apply method (overridden in inherited classes)
+  self$apply <- function(x) return(x) # no change
+
+  return(self)
+}
---
+++ tests/testthat/test_manage_actions.R
@@ -0,0 +1,30 @@
+context("ManageActions")
+
+test_that("initializes with region, population model and apply stages", {
+  TEST_DIRECTORY <- test_path("test_inputs")
+  template <- terra::rast(file.path(TEST_DIRECTORY, "greater_melb.tif"))
+  region <- bsspread::Region(template)
+  stage_matrix <- matrix(c(0.0, 2.0, 5.0,
+                           0.3, 0.0, 0.0,
+                           0.0, 0.6, 0.8),
+                         nrow = 3, ncol = 3, byrow = TRUE)
+  population_model <- bsspread::StagedPopulation(region, stage_matrix)
+  expect_error(manage_actions <- ManageActions(region, "population_model",
+                                               apply_stages = 2:3),
+               paste("Population model must be a 'Population' or inherited",
+                     "class object."))
+  template2 <- template*1
+  template2[1] <- NA
+  expect_error(manage_actions <-
+                 ManageActions(bsspread::Region(template2), population_model,
+                               apply_stages = 2:3),
+               "Population model must be compatible with the region object.")
+  expect_error(manage_actions <- ManageActions(region, population_model,
+                                               apply_stages = 3:4),
+               paste("Apply stages must be a vector of stage indices",
+                     "consistent with the population model."))
+  manage_actions <- ManageActions(region, population_model,
+                                  apply_stages = 2:3) # silent
+  expect_is(manage_actions, "ManageActions")
+  expect_equal(manage_actions$apply(1:10), 1:10) # returns n
+})
